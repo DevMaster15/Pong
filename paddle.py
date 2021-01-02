dfacfec336153bd14046854b49504fc3b94e4876
Maestro <andreamaestro98@gmail.om>
2021-01-02 22:46:24 +0100

    correct show_paddle

--- paddle.py
+++ paddle.py
@@ -2,16 +2,21 @@ import pygame
 
 class Paddle:
 
-    HEIGHT = 80
-    WIDTH = 20
+    HEIGHT = 60
+    WIDTH = 15
 
     def __init__(self, y_pos):
         self.y_pos = y_pos
     
     def show_paddle(self, color, screen, background):
-        pygame.draw.rect(screen, color, pygame.Rect((background.WIDTH-self.WIDTH, self.y-self.HEIGHT/2)))
+
+        print("Y = ")
+        print(self.y_pos)
+
+        pygame.draw.rect(screen, color, pygame.Rect(background.WIDTH-self.WIDTH-10, self.y_pos, self.WIDTH, self.HEIGHT)) # left, top, width, height
     
-    def updateMove(self):
+    def updateMove(self, screen, background):
+        
         self.show_paddle(pygame.Color("black"), screen, background)
-        self.y = pygame.mouse.get_pos()[1] # take the y position, not x
+        self.y_pos = self.y_pos - pygame.mouse.get_pos()[1] # take the y position, not x
         self.show_paddle(pygame.Color("white"), screen, background)
