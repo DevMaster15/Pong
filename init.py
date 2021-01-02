91390b55288ceab39e03081f2b86eadd08a18203
Andrea Maestrutti <andreamaestro98@gmail.com>
2021-01-02 18:22:05 +0100

    create main

---
+++ init.py
@@ -0,0 +1,13 @@
+import pygame
+from display import Display
+from ball import Ball 
+
+def main():
+
+    screen1 = Display(1200, 600, 20)
+    screen1.create_display()
+
+  
+
+if __name__ == "__main__":
+    main()
